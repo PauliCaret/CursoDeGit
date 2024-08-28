9502cfc74c2b7cee4473a291bbc99f4b9f2a71d9
Your Name <you@example.com>
2024-08-28 00:16:24 -0600

    cree html

---
+++ 01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,8 @@
+let numero = 42;
+let nombre = "Hola mundo";
+let verdadero = true;
+let undef;
+let nula = null;
+nombre= 53;
+console .log( typeof nombre);
+console
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,29 @@
+function factorial(num) {
+    let resultado = 1;
+    for (let i = 1; i <= num; i++) {
+        resultado *= i;
+    }
+return resultado;
+}
+
+console.log(factorial(5)); 
+
+fuction fibonacci(n) {
+    if (=== 0) {
+        return 0;
+    
+    } else if (n===1){
+        else{
+            rwturn finonacci(n - 1) + fibonacci (n- 2);
+
+        }
+    
+    }
+
+}
+
+function mostrarSerieFibonacci(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(fibonacci(i));
+    }
+}
